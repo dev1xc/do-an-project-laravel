1ce817c64df38626d576b6bef3a09b68601d4325
dev1xc <56894582+dev1xc@users.noreply.github.com>
2024-01-16 12:05:35 +0700

    update

---
+++ .history/resources/views/frontend/jquery/home.blade_20240116092022.php
@@ -0,0 +1,26 @@
+<script>
+    $(document).ready(function(){
+        $("a.add-to-cart").click(function(){
+            var getId =$(this).attr("id");
+            // ajax(va nó chạy ngầm , giong form:)
+            let countItem = $('div.shop-menu').find('li:eq(3)').find('a').text();
+            countItem ++
+
+            $('div.shop-menu').find('li:eq(3)').find('a').text(countItem)
+            console.log(countItem);
+        //     $.ajax({
+        //         method: "POST",
+        //         url: "ajax-handle-cart.php", //	k co html va chi chay ngầm
+        //         data: {
+        //             xx: getId,
+        //             yy: countItem,
+        //         },
+        //         success : function(res){
+        //             // ket qua ben php tra ve lai
+        //             console.log(res)
+        //             // $("a")
+        //         }
+        //     });
+        // });
+    });
+</script>
---
+++ .history/resources/views/frontend/jquery/home.blade_20240116094438.php
@@ -0,0 +1,78 @@
+<script>
+    let cartCount = 0
+    $(document).ready(function () {
+        let objCha = {}
+        let objCon = {}
+        let cartCount = 0
+        let cartCountObj = {}
+        let slCart = 1
+        $('a.add-to-cart').click(function () {
+            let img = $(this).closest('div.single-products').find('img').attr('src')
+            let price = $(this).closest('div.single-products').find('div.productinfo h2').text()
+            let name = $(this).closest('div.product-overlay').find('p').text()
+            let id = $(this).closest('div.single-products').attr('id')
+            // let id2 = $(this).closest('div.overlay-content').attr('id')
+            objCon = {
+                img: img,
+                price: price,
+                name: name,
+                qty: 1
+            }
+
+            objCha[id] = {...objCon}
+            let temp = 0
+            Object.keys(objCha).map((key, value) => {
+                temp += objCha[key]['qty']
+            })
+                cartCount = temp
+                console.log(cartCount);
+                $('div.shop-menu').find('ul.nav.navbar-nav li:eq(3) a').text(`Cart is ${cartCount}`)
+                cartCountObj = JSON.stringify(cartCount)
+                localStorage.setItem('demSLCart', cartCountObj)
+            let getOBJJSON = localStorage.getItem('baitap')
+            if(getOBJJSON){
+                objCha = JSON.parse(getOBJJSON)
+
+            if (Object.keys(objCha).includes(id)) {
+                let count = objCha[id]['qty']
+                count ++
+                objCha[id]['qty'] = count
+
+                let temp = 0
+            Object.keys(objCha).map((key, value) => {
+                temp += objCha[key]['qty']
+            })
+                cartCount = temp
+                console.log(cartCount);
+                $('div.shop-menu').find('ul.nav.navbar-nav li:eq(3) a').text(`Cart is ${cartCount}`)
+                cartCountObj = JSON.stringify(cartCount)
+                localStorage.setItem('demSLCart', cartCountObj)
+            }else {
+                objCha[id] = {...objCon}
+            }
+            }
+            let objToJson = JSON.stringify(objCha)
+            localStorage.setItem('baitap', objToJson)
+            console.log(objCha)
+        }
+
+        );
+        let getSLCart = localStorage.getItem('demSLCart')
+        if (getSLCart) {
+            slCart = JSON.parse(getSLCart)
+            // let numberslCart = Number(slCart)
+            $('div.shop-menu').find('ul.nav.navbar-nav li:eq(3) a').text(`Cart is ${slCart}`)
+        }
+        $.ajax({
+            type: "GET",
+            url: "/get_product-cart",
+            data: {
+                p_cart: objCha
+            },
+            dataType: "JSOn",
+            success: function (response) {
+
+            }
+        });
+    });
+</script>
